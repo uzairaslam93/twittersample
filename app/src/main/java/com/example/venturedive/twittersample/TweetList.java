13e8ff7d4156e2a4b018c29929032ae7a134d8b6
Uzair Aslam <muhammed.moiz@vistajet.com>
2017-12-23 18:18:36 +0500

    first commit

---
+++ app/src/main/java/com/example/venturedive/twittersample/TweetList.java
@@ -0,0 +1,14 @@
+package com.example.venturedive.twittersample;
+
+import com.twitter.sdk.android.core.models.Tweet;
+
+import java.util.List;
+
+/**
+ * Created by Venturedive on 23/12/2017.
+ */
+
+public class TweetList {
+
+    List <Tweet> Tweets;
+}
